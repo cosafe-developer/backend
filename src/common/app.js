06d2cb8fcdaf8690d6f9b520bc9b546bce3d8b23
Cosafe Dev <cosafeehss@gmail.com>
2025-04-25 14:17:22 -0500

    feat:fix

--- src/common/app.js
+++ src/common/app.js
@@ -1,19 +1,17 @@
 const express = require('express');
 
 
-// const transactionRoutes = require('./routes/transactionRoutes'); // Ruta correcta
 const cors = require('cors');
 const app = express();
 
 // Configurar CORS para permitir solicitudes desde cualquier origen
 app.use(cors());
-// app.use('/backend/webhook', webhookStripe);
+
 
 // Middleware para manejar datos JSON y URL codificada
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-// app.use('/api/v1', transactionRoutes);
 module.exports = app;
 
